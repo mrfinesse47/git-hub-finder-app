aa6e8e20f7174a1239e9b7764811903c9220de82
Kevin Mason <krmason2020@gmail.com>
2022-07-23 13:50:21 -0700

    updates to user item

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 import { useEffect, useState } from "react";
 
 const UserResults = () => {
@@ -22,7 +23,8 @@ const UserResults = () => {
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (
-        <h3 key={user.id}>{user.login}</h3>
+        //<h3 key={user.id}>{user.login}</h3>
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
