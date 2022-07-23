5d1907904f3134787087278a6e2511b7063cc472
Kevin Mason <krmason2020@gmail.com>
2022-07-22 21:22:10 -0700

    made header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button class="btn">click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="Github Finder" />
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
